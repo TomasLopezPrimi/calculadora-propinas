0104b0976e16f06228d02c1bc76227a6bf38d173
Tomas Lopez Primi <primi.tomas@hotmail.com>
2025-07-07 12:10:41 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  console.log(menuItems)
+  const { order, addItem, removeItem} = useOrder()
+
   return (
     <>
       <header className="bg-teal-400 py-6">
@@ -11,16 +14,23 @@ function App() {
       </header>
 
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+
         <div className="space-y-3">
           <h2 className="font-black text-3xl">Menu</h2>
           {menuItems.map(item => (
             <MenuItem 
               key={item.id}
-              item={item} />
+              item={item}
+              addItem={addItem}
+            />
           ))}
         </div>
-        <div>
-          <h2>Consumo</h2>
+
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents
+            order={order}
+            removeItem={removeItem}
+          />
         </div>
       </main>
     </>
